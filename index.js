bbf7bcc1d0af0965ea2e27d49944144ba7f3e721
ashrafulislampro <ashrafulislam1659@gmail.com>
2021-12-11 18:27:00 +0600

    verify id token added

--- index.js
+++ index.js
@@ -1,67 +1,98 @@
-const express = require('express')
-const { MongoClient } = require('mongodb');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId;
-require('dotenv').config();
-const app = express()
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+const admin = require("firebase-admin");
+require("dotenv").config();
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
 
+
+var serviceAccount = require("./full-stack-assignment-10-firebase-adminsdk-ndxig-2341b2a6f7.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q0pfb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_CONNECT}`);
-  const collectionBuyProducts = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_CONNECTED}`);
-  
-  app.post('/addProducts', (req, res)=>{
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client
+    .db(`${process.env.DB_NAME}`)
+    .collection(`${process.env.DB_CONNECT}`);
+  const collectionBuyProducts = client
+    .db(`${process.env.DB_NAME}`)
+    .collection(`${process.env.DB_CONNECTED}`);
+
+  app.post("/addProducts", (req, res) => {
     const products = req.body;
-    collection.insertOne(products)
-    .then(result =>{
+    collection.insertOne(products).then((result) => {
       res.send(result.acknowledged === true);
-    })
+    });
   });
 
-  app.post('/addBuy', (req, res)=>{
+  app.post("/addBuy", (req, res) => {
     const buyProduct = req.body;
-    collectionBuyProducts.insertOne(buyProduct)
-    .then((result)=>{
+    collectionBuyProducts.insertOne(buyProduct).then((result) => {
       res.send(result.acknowledged === true);
-    })
+    });
   });
 
-  app.get('/buyProducts' , (req, res)=>{
-      collectionBuyProducts.find({email : req.query.email})
-      .toArray((err, documents) =>{
-        res.send(documents)
-      })
-  })
+  app.get("/buyProducts", (req, res) => {
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith("Bearer ")) {
+      const idToken = bearer.split(" ")[1];
 
+      admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then((decodedToken) => {
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          if (tokenEmail == queryEmail) {
+            collectionBuyProducts
+              .find({ email: req.query.email })
+              .toArray((err, documents) => {
+                res.send(documents);
+              });
+          }
+        })
+        .catch((error) => {
+          res.status(401).send("UnAuthorized user");
+        });
+    } else {
+      res.status(401).send("UnAuthorized user");
+    }
+  });
 
-  app.get('/products', (req, res)=>{
-    collection.find({})
-    .toArray((err, documents) =>{
+  app.get("/products", (req, res) => {
+    collection.find({}).toArray((err, documents) => {
       res.send(documents);
-    })
+    });
   });
 
-  app.get('/product/:id', (req, res)=>{
-    collection.find({_id : ObjectId(req.params.id)})
-    .toArray((err, documents) =>{
-      res.send(documents[0]);
-    })
+  app.get("/product/:id", (req, res) => {
+    collection
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
   });
+});
 
-
-
-
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello Shop Fresh Valley')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Shop Fresh Valley");
+});
 
 app.listen(port);
